b11cf4e248bfb92208385977adbdcef974ffd344
hahuy <haquanghuy2006abc@gmail.com>
2026-04-22 01:57:36 +0700

    bt

---
+++ src/test/java/com/example/java3ss12/Java3ss12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java3ss12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtitCntt2It210Ss12Ex1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
